b42987910b48d7ef0f0c48e7cafbfb93a9867525
Stuart Powers <stuart.powers@gmail.com>
2012-01-06 08:25:44 -0500

    added a status indicator to the Stats model

--- mare/models/stats.py
+++ mare/models/stats.py
@@ -8,14 +8,16 @@ class Stats(db.Model):
     user_uid = db.Column(db.Integer, db.ForeignKey('user.uid'))
     video_id = db.Column(db.Integer, db.ForeignKey('video.id'))
     watched = db.Column(db.Integer)
+    status = db.Column(db.Integer)
     user = db.relationship("User")
     video = db.relationship("Video")
 
     def __init__(self, user, video):
         self.user = user
         self.video = video
+        self.status = 0
         self.watched = 0
 
     def __repr__(self):
-        return "<Stats('%r:%r:%d')>" % (self.user.name,self.video.module,self.watched)
+        return "<Stats('%r:%r:%d')>" % (self.user.name, self.video.module, self.status, self.watched)
 
